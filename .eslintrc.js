cd41cda8df71e8a3c3310ce107df0bc16359fbc2
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-07-16 21:57:50 +0200

    Configure template for browser

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,43 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:prettier/recommended',
+	],
+	overrides: [],
+	ignorePatterns: ['*.html'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint', 'prettier'],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-this-alias': 'off',
+		'@typescript-eslint/ban-ts-comment': 'off',
+		'@typescript-eslint/no-empty-interface': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+				useTabs: true,
+				singleQuote: true,
+				printWidth: 100,
+				singleAttributePerLine: true,
+			},
+		],
+	},
 };
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-export const delayMillis = (delayMs: number): Promise<void> => new Promise(resolve => setTimeout(resolve, delayMs));
+/**
+ * This file is the entrypoint of browser builds.
+ * The code executes when loaded in a browser.
+ */
+import { foo } from './foo'
 
-export const greet = (name: string): string => `Hello ${name}`
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).foo = foo  // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
 
-export const foo = async (): Promise<boolean> => {
-  console.log(greet('World'))
-  await delayMillis(1000)
-  console.log('done')
-  return true
-}
+console.log('Method "foo" was added to the window object. You can try it yourself by just entering "await foo()"')
