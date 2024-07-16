7cf074b4a4ba1f907ad29fa877272beca94e9d80
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-07-16 23:39:59 +0200

    Generate notation

--- src/UIManager.ts
+++ src/UIManager.ts
@@ -13,10 +13,12 @@ export class UIManager {
 
 	private generateBoard() {
 		const board = document.getElementById('board')!;
+
 		for (let x = 0; x < 8; x++) {
 			const row = document.createElement('div');
 			row.classList.add('row');
 			row.id = `row-${x}`;
+			board.appendChild(row);
 			for (let y = 0; y < 8; y++) {
 				const tile = document.createElement('div');
 				tile.classList.add('tile');
@@ -26,8 +28,21 @@ export class UIManager {
 					tile.classList.add('tile-variant');
 				}
 				row.appendChild(tile);
+
+				if (y == 0) {
+					const notation = document.createElement('div');
+					notation.classList.add('notation', 'notation-top');
+					notation.textContent = `${x + 1}`;
+					tile.appendChild(notation);
+				}
+				if (x == 7) {
+					const letters = 'abcdefgh';
+					const notation = document.createElement('div');
+					notation.classList.add('notation', 'notation-bot');
+					notation.textContent = letters.split('')[y];
+					tile.appendChild(notation);
+				}
 			}
-			board.appendChild(row);
 		}
 	}
 }
