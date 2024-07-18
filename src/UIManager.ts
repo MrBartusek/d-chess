cec141f5908ac7c96aafeeea676a4066b07ff61b
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-07-18 17:26:41 +0200

    More UIManager

--- src/UIManager.ts
+++ src/UIManager.ts
@@ -1,48 +1,66 @@
+enum NotationPosition {
+	TOP = 'notation-top',
+	BOTTOM = 'notation-bot',
+}
+
 export class UIManager {
-	private started = false;
-
 	public start() {
-		if (this.started) {
-			throw new Error('UIManager is already started');
-		}
-
-		this.generateBoard();
-
-		this.started = true;
+		const container = document.getElementById('board')!;
+		this.generateBoard(container);
 	}
 
-	private generateBoard() {
-		const board = document.getElementById('board')!;
-
+	private generateBoard(container: HTMLElement) {
 		for (let x = 0; x < 8; x++) {
-			const row = document.createElement('div');
-			row.classList.add('row');
-			row.id = `row-${x}`;
-			board.appendChild(row);
+			const row = this.createRow(x);
+			container.appendChild(row);
+
 			for (let y = 0; y < 8; y++) {
-				const tile = document.createElement('div');
-				tile.classList.add('tile');
-				tile.id = `tile-${x * 8 + y}`;
-				const isVariant = (x + y) % 2;
-				if (isVariant) {
-					tile.classList.add('tile-variant');
-				}
+				const tile = this.createTile(x, y);
 				row.appendChild(tile);
 
+				if (x == 1 || x == 6) {
+					const figure = document.createElement('img');
+					figure.src = 'static/img/pieces/wP.png';
+					figure.classList.add('figure');
+					tile.appendChild(figure);
+				}
 				if (y == 0) {
-					const notation = document.createElement('div');
-					notation.classList.add('notation', 'notation-top');
-					notation.textContent = `${x + 1}`;
+					const notation = this.createNotation(`${x + 1}`, NotationPosition.TOP);
 					tile.appendChild(notation);
 				}
 				if (x == 7) {
-					const letters = 'abcdefgh';
-					const notation = document.createElement('div');
-					notation.classList.add('notation', 'notation-bot');
-					notation.textContent = letters.split('')[y];
+					const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+					const notation = this.createNotation(letters[y], NotationPosition.BOTTOM);
 					tile.appendChild(notation);
 				}
 			}
 		}
+	}
+
+	private createRow(x: number): HTMLDivElement {
+		const row = document.createElement('div');
+		row.classList.add('row');
+		row.id = `row-${x}`;
+		return row;
+	}
+
+	private createTile(x: number, y: number): HTMLDivElement {
+		const tile = document.createElement('div');
+		tile.classList.add('tile');
+		tile.id = `tile-${x * 8 + y}`;
+
+		const isVariant = (x + y) % 2;
+		if (isVariant) {
+			tile.classList.add('tile-variant');
+		}
+
+		return tile;
+	}
+
+	private createNotation(text: string, position: NotationPosition): HTMLDivElement {
+		const notation = document.createElement('div');
+		notation.classList.add('notation', position);
+		notation.textContent = text;
+		return notation;
 	}
 }
