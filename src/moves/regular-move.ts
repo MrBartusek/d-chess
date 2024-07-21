7c1ca75e8e1a64e91722857a9f000b067f32f3f3
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-07-21 21:04:30 +0200

    Wrap regular moves

--- src/moves/regular-move.ts
+++ src/moves/regular-move.ts
@@ -2,12 +2,19 @@ import { Move } from '../base/moving-strategy';
 import { Board } from '../board';
 
 export class RegularMove extends Move {
-	constructor(protected tiles: number) {
+	constructor(protected step: number) {
 		super();
 	}
 
 	computeMoves(_board: Board, position: number): number[] {
-		const newPosition = position + this.tiles;
+		const newPosition = position + this.step;
+
+		const currentRow = position % 8;
+		const nextRow = newPosition % 8;
+
+		if (Math.abs(currentRow - nextRow) > 2) {
+			return [];
+		}
 
 		if (newPosition >= 0 && newPosition <= 63) {
 			return [newPosition];
